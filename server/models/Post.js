c8ff72df77354dfa9f5b072dfaf9827240fb97c2
rithesh-n <ritheshan18@gmail.com>
2025-06-08 23:57:26 +0530

    feat: add community features with post sharing

--- server/models/Post.js
+++ server/models/Post.js
@@ -4,8 +4,24 @@ const postSchema = new mongoose.Schema(
   {
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     text: { type: String },
-    image: { type: String },
-    video: { type: String },
+    image: {
+      type: String,  // Will store Cloudinary URL
+      validate: {
+        validator: function (v) {
+          return !v || v.startsWith('https://res.cloudinary.com/');
+        },
+        message: 'Invalid image URL'
+      }
+    },
+    video: {
+      type: String,  // Will store Cloudinary URL
+      validate: {
+        validator: function (v) {
+          return !v || v.startsWith('https://res.cloudinary.com/');
+        },
+        message: 'Invalid video URL'
+      }
+    },
     tags: [String],
     type: { type: String, default: 'post' }, // distinguishes from reels
     upvotes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
@@ -18,5 +34,20 @@ const postSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+postSchema.pre('remove', async function(next) {
+  try {
+    if (this.image) {
+      const publicId = this.image.split('/').slice(-1)[0].split('.')[0];
+      await cloudinary.uploader.destroy(`agrimaster/posts/images/${publicId}`);
+    }
+    if (this.video) {
+      const publicId = this.video.split('/').slice(-1)[0].split('.')[0];
+      await cloudinary.uploader.destroy(`agrimaster/posts/videos/${publicId}`, { resource_type: 'video' });
+    }
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 const Post = mongoose.model('Post', postSchema);
 export default Post;
