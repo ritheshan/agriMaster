18e05318aa4998ebefa1ae9d0ba90d0141c56bb4
rithesh-n <ritheshan18@gmail.com>
2025-06-17 01:57:46 +0530

    chore(server): update app bootstrap logic to register new middleware and config

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import connectDB from './config/db.js'; // Make sure the `.js` extension is included
 import authRoutes from './routes/auth.js';
 import userRoutes from './routes/user.js';
 
@@ -11,13 +12,14 @@ dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
 
+// Connect to MongoDB
+await connectDB(); // top-level await is allowed in ES modules
+
+
+
 app.use('/auth', authRoutes);
 app.use('/me', userRoutes);
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    app.listen(process.env.PORT || 5000, () => {
-      console.log('Server running');
-    });
-  })
-  .catch((err) => console.error(err));
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
