f352bbcfaebe467168a6b9eb7f686c21389f1758
rithesh-n <ritheshan18@gmail.com>
2025-06-17 21:35:02 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { ALL } from 'dns';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ALL,
     open: true, // automatically open the browser
   },
 });
