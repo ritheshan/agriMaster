3c42ed783b8c6e2dba41ef5aad3c78dee1e8e2e8
rithesh-n <ritheshan18@gmail.com>
2025-06-18 00:40:05 +0530

    feat(auth): add Google OAuth route handlers

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import { sendOTP, verifyOTPController , getMe} from '../controllers/authController.js';
+import { sendOTP, verifyOTPController , getMe ,  getGoogleAuthURL,
+  handleGoogleCallback} from '../controllers/authController.js';
 const authMiddleware = require('../middleware/authMiddleware');
 const rateLimiter = require('../middleware/rateLimiter');
 
@@ -7,5 +8,7 @@ const router = express.Router();
 router.get('/me', authMiddleware, getMe);
 router.post('/send-otp', sendOTP);
 router.post('/verify-otp', verifyOTPController);
+router.get('/google/url', getGoogleAuthURL);
+router.get('/google/callback', handleGoogleCallback);
 
 export default router;
